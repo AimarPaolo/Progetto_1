b2ce3d6919d4fb50262a35353698e41644468e18
Paolo Aimar <s297424@studenti.polito.it>
2024-05-23 11:50:53 +0200

    Creazione di una pagina Registrazione (non ancora funzionante, si dovrebbe implementare attraverso i cookies). Grafica ancora da sistemare, utilizzando comandi e uniformando la grafica della pagina di login con quella della pagina di registrazione.

--- javascript_ESP1.js
+++ javascript_ESP1.js
@@ -1,13 +1,11 @@
 //ricevo l'id del form e controllo che i dati inseriti siano corretti, ad esempio che il nome non sia troppo corto, 
 //che la mail sia valida (anche se un primo controllo dovrebbe esser
 // già stato effettuato da input type email) ... 
-
+"use strict";
 function validateForm(f1){
-    let nome = document.getElementById(f1).nome.value;
-    let cognome = document.getElementById(f1).cognome.value;
     let email = document.getElementById(f1).email.value;
-    let textArea = document.getElementById(f1).message.value;
-    if (nome != "" && cognome != "" && email != "" && textArea != ""){
+    let password = document.getElementById(f1).pwrd.value;
+    if (email != "" && pswrd != ""){
         console.log("valori inseriti correttamente, necessario solo controllare che l'email rispetti lo stile");
         let regexp_email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         if(regexp_email.test(email)){
@@ -19,15 +17,15 @@ function validateForm(f1){
         }
     }else{
         // Se un campo obbligatorio non è stato compilato, visualizza un messaggio di errore distinto per ogni tipo di "errore"
-        if (nome === "") {
-            window.alert("Per favore, inserisci il tuo nome.");
-        } else if (cognome === "") {
-            window.alert("Per favore, inserisci il tuo cognome.");
+        if (pswrd === "") {
+            window.alert("Per favore, inserisci la password.");
         } else if (email === "") {
             window.alert("Per favore, inserisci la tua email.");
-        } else if (textArea === "") {
-            window.alert("Per favore, inserisci il tuo messaggio.");
         }
         return false;
     }
+}
+function openSubscription(){
+    //utilizzo questo comando per reindirizzare la pagina HTML dalla pagina accedi alla pagina di registrazione
+    window.location.href = 'Registrazione_ESP1.html';
 }
